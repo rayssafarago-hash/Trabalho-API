3f0c03c5b36139a469c1a476f6f88a0c324b62f0
rayssafarago <rayssa.farago@escola.pr.gov.br>
2026-03-24 10:31:45 -0300

    primeiro commit

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ const fs = require ('fs');
 const app = express();
 const PORT = 3000;
 
-app.use (express.json())
+app.use (express.json());
+
+app.use('/produtos', rotasProdutos);
+
+app.use('/', (rreq, res)=>{
+    res.json({
+        mensagem: 'API de estoque Rodando',
+        endpoints: {
+            listarTodos: 'GET /produtos',
+            buscarPorId: 'GET /produtos/:id',
+            cadastrar: 'POST /produtos',
+            atualizar: 'PUT /produtos/:id',
+            remover: 'DELETE /produto/:id'
+
+        }
+    });
+});
 
 
 app.listen(PORT, () => {
