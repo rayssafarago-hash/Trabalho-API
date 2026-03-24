c7fbe13811ac3962ce09ed331a4e2cb68b4d8810
rayssafarago <rayssa.farago@escola.pr.gov.br>
2026-03-24 10:30:00 -0300

    Trabalho api

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fs = require ('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use (express.json())
+
+
+app.listen(PORT, () => {
+    console.log
+    (`Servidor rodando em http://localhost:${PORT}`)
+});
